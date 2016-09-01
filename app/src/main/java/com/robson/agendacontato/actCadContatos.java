0c105b2501bcef2331ea267a082422963ba692a8
Robson Cidão <robsoncidao@gmail.com>
2016-09-01 15:48:55 -0300

    Campos e Spinner da tela cadastro de contato criados

--- app/src/main/java/com/robson/agendacontato/actCadContatos.java
+++ app/src/main/java/com/robson/agendacontato/actCadContatos.java
@@ -22,7 +22,7 @@ public class actCadContatos extends AppCompatActivity {
     Spinner spnTipoDataEspeciais;
 
     //Armazenar os dados digitados no ArrayAdapter
-    ArrayAdapter<String> adpTipEmail;
+    ArrayAdapter<String> adpTipoEmail;
     ArrayAdapter<String> adpTipoTelefone;
     ArrayAdapter<String>adpTipoEndereco;
     ArrayAdapter<String>adpTipoDatasEspeciais;
@@ -39,28 +39,50 @@ public class actCadContatos extends AppCompatActivity {
         edtEndereco      = (EditText)findViewById(R.id.edtEndereco);
         edtDataEspeciais = (EditText)findViewById(R.id.edtDatasEspeciais);
         edtGrupos        = (EditText)findViewById(R.id.edtGrupos);
+
         //Iniciando as opções de cada campo da agenda
         spnTipoTelefone  = (Spinner)findViewById(R.id.spnTipoTelefone);
         spnTipoEmail     = (Spinner)findViewById(R.id.spntTipEmail);
         spnTipoEndereco  = (Spinner)findViewById(R.id.spnTipoEndereco);
         spnTipoDataEspeciais = (Spinner)findViewById(R.id.spnTipoDatasEspeciais);
+
         //Iniciando os ArrayAdapter<>
-        adpTipEmail           = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item);
-        adpTipEmail.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adpTipoEmail           = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item);
+        adpTipoEmail.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         adpTipoTelefone       = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item);
         adpTipoTelefone.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         adpTipoEndereco       = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item);
-        adpTipoEndereco.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item;
+        adpTipoEndereco.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         adpTipoDatasEspeciais = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item);
         adpTipoDatasEspeciais.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        //Associando as opções de cada ArrayAdapter para cada spinner
-        spnTipoEmail.setAdapter(adpTipEmail);
-        spnTipoEndereco.setAdapter(adpTipoEndereco);
+        //Associando o Spnnier ao ArrayAdapter
+        spnTipoEmail.setAdapter(adpTipoEmail);
+        spnTipoTelefone.setAdapter(adpTipoTelefone);
         spnTipoEndereco.setAdapter(adpTipoEndereco);
         spnTipoDataEspeciais.setAdapter(adpTipoDatasEspeciais);
+
+        //Adicionando os itens da cada Spnnier
+        adpTipoEmail.add("Casa");
+        adpTipoEmail.add("Trabalho");
+        adpTipoEmail.add("Outros");
+
+        adpTipoTelefone.add("Celular");
+        adpTipoTelefone.add("Trabalho");
+        adpTipoTelefone.add("Casa");
+        adpTipoTelefone.add("Principal");
+
+        adpTipoEndereco.add("Casa");
+        adpTipoEndereco.add("Trabalho");
+        adpTipoEndereco.add("Outros");
+
+        adpTipoDatasEspeciais.add("Aniversário");
+        adpTipoDatasEspeciais.add("Data comemorativa");
+        adpTipoDatasEspeciais.add("Outros");
+
+
     }
 }
